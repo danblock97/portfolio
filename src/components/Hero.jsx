4b990a8b85270e19751708d562eaa92ceb9dc35c
Dan <generalgoldiez@gmail.com>
2023-06-19 10:28:15 +0100

    Updated Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,9 +21,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Dan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop fullstack .NET applications{" "}
-            <br className="sm:block hidden" /> as well as modern user
-            interfaces.
+            I develop frontend applications <br className="sm:block hidden" />{" "}
+            as well as Jira Projects for a large organisation.
           </p>
         </div>
       </div>
