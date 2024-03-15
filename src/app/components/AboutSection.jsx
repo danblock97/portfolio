161ddaa2461005d4f04275281deda64cfb34a80f
danblock97 <generalgoldiez@gmail.com>
2024-03-15 10:51:11 +0000

    Added About Me section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+
+const tab_data = [
+	{
+		title: "Skills",
+		id: "skills",
+		content: (
+			<ul className="list-disc pl-2">
+				<li>Node.js</li>
+				<li>Axios</li>
+				<li>NextJS</li>
+				<li>React</li>
+				<li>.NET</li>
+				<li>Javascript</li>
+			</ul>
+		),
+	},
+	{
+		title: "Qualifications",
+		id: "qualifications",
+		content: (
+			<ul className="list-disc pl-2">
+				<li>MBCS</li>
+				<li>IT Practitioners</li>
+				<li>C# Foundations</li>
+			</ul>
+		),
+	},
+	{
+		title: "Experience",
+		id: "experience",
+		content: (
+			<ul className="list-disc pl-2">
+				<li>.NET Full Stack Software Engineer</li>
+				<li>Jira/Confluence Admin & Configurator</li>
+				<li>Oracle Apex Developer</li>
+				<li>React/NextJS Web Applications</li>
+			</ul>
+		),
+	},
+];
+
+const AboutSection = () => {
+	const [tab, setTab] = useState("skills");
+	const [isPending, startTransition] = useTransition();
+
+	const handleTabChange = (id) => {
+		startTransition(() => {
+			setTab(id);
+		});
+	};
+	return (
+		<section className="text-white">
+			<div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+				<Image
+					src="/images/about-image.png"
+					alt="about image"
+					height={500}
+					width={500}
+				/>
+				<div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+					<h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+					<p className="text-base lg:text-lg">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+						nobis libero soluta tempora dolorum eum maxime laboriosam odio,
+						alias assumenda omnis aliquam aut a, explicabo sapiente eveniet
+						aspernatur impedit magni.
+					</p>
+					<div className="flex flex-row mt-8">
+						<TabButton
+							selectTab={() => handleTabChange("skills")}
+							active={tab === "skills"}
+						>
+							Skills
+						</TabButton>
+						<TabButton
+							selectTab={() => handleTabChange("qualifications")}
+							active={tab === "qualifications"}
+						>
+							Qualifications
+						</TabButton>
+						<TabButton
+							selectTab={() => handleTabChange("experience")}
+							active={tab === "experience"}
+						>
+							Experience
+						</TabButton>
+					</div>
+					<div className="mt-8">
+						{tab_data.find((t) => t.id === tab).content}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default AboutSection;
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,14 +10,12 @@ const HeroSection = () => {
 			<div className="grid grid-cols-1 sm:grid-cols-12">
 				<div className="col-span-7 place-self-center text-center sm:text-left">
 					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-						<span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-							Hello, I'm{" "}
+						<span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-pink-500">
+							Hello, I'm Dan{" "}
 						</span>
 						<br />
 						<TypeAnimation
 							sequence={[
-								"Dan",
-								1000,
 								"Web Developer",
 								1000,
 								"Mobile Developer",
@@ -35,10 +33,10 @@ const HeroSection = () => {
 						sunt in unde deserunt cum, ipsa quod dolorem assumenda et eaque
 						labore iure vel molestiae sequi asperiores, illo, optio soluta.
 					</p>
-					<button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+					<button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-amber-500 to-pink-500 hover:bg-slate-200 text-white">
 						Hire Me
 					</button>
-					<button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+					<button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-amber-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
 						<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
 							Download CV
 						</span>
